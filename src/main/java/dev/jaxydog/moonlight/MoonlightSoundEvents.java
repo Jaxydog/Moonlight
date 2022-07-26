ddf5f8a91f60f78805cad4f719c11b51cff08bf5
jaxydog <32988901+jaxydog@users.noreply.github.com>
2022-07-26 16:33:50 -0400

    Update sound event class

--- src/main/java/dev/jaxydog/moonlight/MoonlightSoundEvents.java
+++ src/main/java/dev/jaxydog/moonlight/MoonlightSoundEvents.java
@@ -3,15 +3,18 @@ package dev.jaxydog.moonlight;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.registry.Registry;
 
+/** Moonlight sound event class */
 public class MoonlightSoundEvents {
+	/** Avocado jingle sound */
 	public static final SoundEvent ITEM_AVOCADO_JINGLE = new SoundEvent(Moonlight.id("item.avocado.jingle"));
+	/** Snake alert sound */
 	public static final SoundEvent ORIGIN_SNAKE_ALERT = new SoundEvent(Moonlight.id("origin.snake.alert"));
+	/** Snake sneak sound */
 	public static final SoundEvent ORIGIN_SNAKE_SNEAK = new SoundEvent(Moonlight.id("origin.snake.sneak"));
 
-	private static void register(SoundEvent... sounds) {
-		for (var sound : sounds) {
-			Registry.register(Registry.SOUND_EVENT, sound.getId(), sound);
-		}
+	/** Registers a sound */
+	private static void register(SoundEvent sound) {
+		Registry.register(Registry.SOUND_EVENT, sound.getId(), sound);
 	}
 
 	public static void register() {
