8cd29565d38a439a1195c0cbc9d421678d1f594a
jaxydog <32988901+jaxydog@users.noreply.github.com>
2023-01-06 22:16:21 -0500

    Remake scythe

--- src/main/java/dev/jaxydog/moonlight/item/unique/ScytheItem.java
+++ src/main/java/dev/jaxydog/moonlight/item/unique/ScytheItem.java
@@ -1,29 +1,62 @@
 package dev.jaxydog.moonlight.item.unique;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.mojang.datafixers.util.Pair;
 import dev.jaxydog.moonlight.Moonlight;
 import dev.jaxydog.moonlight.item.MLItem.Config;
 import dev.jaxydog.moonlight.utility.Registerable;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.yarn.constants.MiningLevels;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.client.resource.language.I18n;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.HoeItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.Items;
+import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterials;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.BinomialLootNumberProvider;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
-public class ScytheItem extends HoeItem implements Registerable {
+public class ScytheItem extends SwordItem implements Registerable {
+
+	protected static final Map<Block, Pair<Predicate<ItemUsageContext>, Consumer<ItemUsageContext>>> TILLING_ACTIONS = Maps.newHashMap(
+		ImmutableMap.of(
+			Blocks.GRASS_BLOCK,
+			Pair.of(HoeItem::canTillFarmland, HoeItem.createTillAction(Blocks.FARMLAND.getDefaultState())),
+			Blocks.DIRT_PATH,
+			Pair.of(HoeItem::canTillFarmland, HoeItem.createTillAction(Blocks.FARMLAND.getDefaultState())),
+			Blocks.DIRT,
+			Pair.of(HoeItem::canTillFarmland, HoeItem.createTillAction(Blocks.FARMLAND.getDefaultState())),
+			Blocks.COARSE_DIRT,
+			Pair.of(HoeItem::canTillFarmland, HoeItem.createTillAction(Blocks.DIRT.getDefaultState())),
+			Blocks.ROOTED_DIRT,
+			Pair.of(c -> true, HoeItem.createTillAndDropAction(Blocks.DIRT.getDefaultState(), Items.HANGING_ROOTS))
+		)
+	);
 
 	protected final Config _CONFIG;
 
@@ -93,9 +126,67 @@ public class ScytheItem extends HoeItem implements Registerable {
 	}
 
 	@Override
+	public boolean isSuitableFor(BlockState state) {
+		int i = this.getMaterial().getMiningLevel();
+
+		if (i < MiningLevels.DIAMOND && state.isIn(BlockTags.NEEDS_DIAMOND_TOOL)) {
+			return super.isSuitableFor(state);
+		}
+		if (i < MiningLevels.IRON && state.isIn(BlockTags.NEEDS_IRON_TOOL)) {
+			return super.isSuitableFor(state);
+		}
+		if (i < MiningLevels.STONE && state.isIn(BlockTags.NEEDS_STONE_TOOL)) {
+			return super.isSuitableFor(state);
+		}
+
+		return state.isIn(BlockTags.HOE_MINEABLE) || super.isSuitableFor(state);
+	}
+
+	@Override
 	public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
 		target.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 100, 0, false, true), attacker);
 
 		return super.postHit(stack, target, attacker);
+	}
+
+	@Override
+	public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+		if (!world.isClient && state.getHardness(world, pos) != 0.0f) {
+			stack.damage(1, miner, e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+		}
+
+		return true;
+	}
+
+	@Override
+	public ActionResult useOnBlock(ItemUsageContext context) {
+		BlockPos pos;
+		World world = context.getWorld();
+		var pair = TILLING_ACTIONS.get(world.getBlockState(pos = context.getBlockPos()).getBlock());
+
+		if (pair == null) {
+			return ActionResult.PASS;
+		}
+
+		var predicate = pair.getFirst();
+		var consumer = pair.getSecond();
+
+		if (predicate.test(context)) {
+			var player = context.getPlayer();
+
+			world.playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0f, 1.0f);
+
+			if (!world.isClient) {
+				consumer.accept(context);
+
+				if (player != null) {
+					context.getStack().damage(1, player, p -> p.sendToolBreakStatus(context.getHand()));
+				}
+			}
+
+			return ActionResult.success(world.isClient);
+		}
+
+		return ActionResult.PASS;
 	}
 }
