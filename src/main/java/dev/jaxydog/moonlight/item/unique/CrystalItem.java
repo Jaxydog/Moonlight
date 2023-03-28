7ce16f40e018761b7f3017b8034e821707a533fb
jaxydog <32988901+jaxydog@users.noreply.github.com>
2023-03-28 17:13:58 -0400

    Update crystal crafting, add dep. to nbt crafting

--- src/main/java/dev/jaxydog/moonlight/item/unique/CrystalItem.java
+++ src/main/java/dev/jaxydog/moonlight/item/unique/CrystalItem.java
@@ -5,11 +5,20 @@ import dev.jaxydog.moonlight.item.MLItem;
 import java.util.List;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 
 public class CrystalItem extends MLItem implements Emotional {
+
+	public static final DamageSource BOTTLE_UP = new DamageSource("axolotlBottleUp")
+		.setBypassesArmor()
+		.setBypassesProtection()
+		.setUnblockable()
+		.setUsesMagic();
+	public static final String CRAFT_COMMAND = "function moonlight:axolotl/craft_crystal";
 
 	public CrystalItem(Settings settings, Config config) {
 		super(settings, config);
@@ -37,6 +46,21 @@ public class CrystalItem extends MLItem implements Emotional {
 		var emotion = Text.translatable(Emotional.getEmotion(stack).getTranslationKey());
 
 		return Text.translatable(this.getTranslationKey()).append(" ").append(emotion);
+	}
+
+	@Override
+	public void onCraft(ItemStack stack, World world, PlayerEntity player) {
+		var server = player.world.getServer();
+
+		if (server != null) {
+			var source = player.getCommandSource().withSilent();
+
+			server.getCommandManager().executeWithPrefix(source, CRAFT_COMMAND);
+		}
+
+		player.damage(BOTTLE_UP, player.getMaxHealth() / 2.0f);
+
+		super.onCraft(stack, world, player);
 	}
 
 	@Override
---
+++ src/main/resources/data/moonlight/functions/axolotl/craft_crystal.mcfunction
@@ -0,0 +1,7 @@
+title @s actionbar {"translate": "power.moonlight.axolotl.axocerias_blessing.craft_crystal", "color": "yellow"}
+playsound minecraft:block.amethyst_block.chime player @a ~ ~ ~ 1 0.75
+playsound minecraft:block.amethyst_block.chime player @a ~ ~ ~ 1 1
+playsound minecraft:block.amethyst_block.chime player @a ~ ~ ~ 1 1.25
+particle minecraft:end_rod ~ ~0.25 ~ 0.2 0.2 0.2 0 10 normal @a
+effect give @s minecraft:slowness 1 0 true
+clear @s minecraft:prismarine_shard 4
